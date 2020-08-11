9111bb83815cb75809b77bbf40f7e73b2520a2eb
Marco Laspe <marco@rockiger.com>
2020-08-11 05:57:15 +0200

    Add BoardsPage container and create a base menu to jump between pages.

--- src/types/RootState.ts
+++ src/types/RootState.ts
@@ -1,3 +1,4 @@
+import { BoardsPageState } from 'app/containers/BoardsPage/types';
 // [IMPORT NEW CONTAINERSTATE ABOVE] < Needed for generating containers seamlessly
 
 /* 
@@ -5,5 +6,6 @@
   You have to declare them here manually
 */
 export interface RootState {
+  boardsPage?: BoardsPageState;
   // [INSERT NEW REDUCER KEY ABOVE] < Needed for generating containers seamlessly
 }
