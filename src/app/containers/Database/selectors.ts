f87bb6c85b3cb83a7c6930e7aedb2ec71fc1bf1a
Marco Laspe <marco@rockiger.com>
2020-08-11 23:23:53 +0200

    Make private route private.

--- src/app/containers/Database/selectors.ts
+++ src/app/containers/Database/selectors.ts
@@ -1,6 +1,7 @@
 import { createSelector } from '@reduxjs/toolkit';
-
+import _ from 'lodash';
 import { RootState } from 'types';
+
 import { initialState } from './slice';
 
 const selectDomain = (state: RootState) => state.database || initialState;
@@ -8,4 +9,9 @@ const selectDomain = (state: RootState) => state.database || initialState;
 export const selectDatabase = createSelector(
   [selectDomain],
   databaseState => databaseState,
+);
+
+export const selectIsAuthenticated = createSelector(
+  [selectDomain],
+  databaseState => !_.isEmpty(databaseState.authUser),
 );
--- src/app/containers/Database/types.ts
+++ src/app/containers/Database/types.ts
@@ -1,4 +1,6 @@
 /* --- STATE --- */
-export interface DatabaseState {}
+export interface DatabaseState {
+  authUser: { [key: string]: any };
+}
 
 export type ContainerState = DatabaseState;
