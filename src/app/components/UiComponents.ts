c3468d9d6a7130e92c7971d08657a995be74e42a
Marco Laspe <marco.laspe@merkurist.de>
2020-08-31 22:58:26 +0200

    Base the button component on Reaktit's Button.

--- src/app/components/UiComponents.ts
+++ src/app/components/UiComponents.ts
@@ -1,3 +1,4 @@
+import { Button as ReaButton } from 'reakit/Button';
 import styled from 'styled-components/macro';
 import media from 'styled-media-query';
 
@@ -22,7 +23,7 @@ export const PageTitle = styled.h1`
   margin: 0;
 `;
 
-export const Content = styled(Horizontal)`
+export const Content = styled.div`
   align-items: flex-start;
   gap: 1.5rem;
   justify-content: space-evenly;
@@ -36,3 +37,41 @@ export const Content = styled(Horizontal)`
 export const Spacer = styled.div`
   flex-grow: 1;
 `;
+
+export const Card = styled.div`
+  background-color: white;
+  border: 1px solid ${p => p.theme.palette.grey[300]};
+  border-radius: 4px;
+  padding: 1rem;
+`;
+
+export const Button = styled(ReaButton)`
+  background-image: none;
+  background-color: #fff;
+  border: 1px solid ${p => p.theme.palette.grey[300]};
+  border-radius: 2px;
+  color: ${p => p.theme.palette.text.primary};
+  cursor: pointer;
+  display: inline-block;
+  font-weight: 400;
+  font-size: 0.875rem;
+  height: 32px;
+  line-height: 1.5rem;
+  padding: 4px 15px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  transition: all 0.3s cubic-bezier(0.645, 0.045, 0.355, 1);
+  user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+  &:hover {
+    color: ${p => p.theme.palette.primary.main};
+    border-color: ${p => p.theme.palette.primary.main};
+  }
+  &:focus {
+    box-shadow: 0 0 0 0.2em rgba(0, 109, 255, 0.4);
+  }
+`;
+
+export const A = styled.a``;
