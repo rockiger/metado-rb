4f9325186cfceeebf2aa1eb5d38b3df683699674
Marco Laspe <marco.laspe@merkurist.de>
2020-09-02 12:37:51 +0200

    Sicherung und Step 1, load repos for step 2

--- src/app/containers/AddGithubRepo/slice.ts
+++ src/app/containers/AddGithubRepo/slice.ts
@@ -3,13 +3,25 @@ import { createSlice } from 'utils/@reduxjs/toolkit';
 import { ContainerState } from './types';
 
 // The initial state of the AddGithubRepo container
-export const initialState: ContainerState = {};
+export const initialState: ContainerState = {
+  repos: [],
+  status: 'idle',
+};
 
 const addGithubRepoSlice = createSlice({
   name: 'addGithubRepo',
   initialState,
   reducers: {
-    someAction(state, action: PayloadAction<any>) {},
+    fetchGithubRepos(state, action: PayloadAction<{ githubToken: string }>) {
+      state.status = 'fetching';
+    },
+    fetchGithubReposError(state, action: PayloadAction<{ error: Error }>) {
+      state.status = 'error';
+    },
+    fetchGithubReposSuccess(state, action: PayloadAction<{ repos: any[] }>) {
+      state.status = 'success';
+      state.repos = action.payload.repos;
+    },
   },
 });
 
