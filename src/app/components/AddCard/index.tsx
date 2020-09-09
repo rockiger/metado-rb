e3083de1653f7f2da522d00fa8cad8e4a37a65b6
Marco Laspe <marco.laspe@merkurist.de>
2020-09-09 09:32:43 +0200

    Refactor Board Into Column

---
+++ src/app/components/AddCard/index.tsx
@@ -0,0 +1,15 @@
+/**
+ *
+ * AddCard
+ *
+ */
+import React from 'react';
+import styled from 'styled-components/macro';
+
+interface Props {}
+
+export function AddCard(props: Props) {
+  return <Div></Div>;
+}
+
+const Div = styled.div``;
